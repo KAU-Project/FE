28ebfc9d8cce2875631f542f9323304ca2003d13
SangHun-OH <oshosh12@gmail.com>
2021-05-26 18:34:59 +0900

    18:34

---
+++ src/Aboutblue.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './AboutBlue.module.scss';
+
+const Aboutblue = (
+) => {
+  return (<div className={styles.aboutBlueTwo}><img alt="" className={styles.image3} src="https://static.overlay-tech.com/assets/2fc8da7d-5731-468a-917a-def1e24b423e.png"/>
+    <div className={styles.frame108}><p className={styles.aboutBlue}>ABOUT BLUE</p><p className={styles.sexMaster}>더 나은 설문조사 경험을 위해<br />
+    많은 것을 준비했습니다.</p><p className={styles.blueText}>블루는 여러 서베이들을 조사하고, 그들의 장점은 가져오고 단점은 보완하였습니다.<br />
+    우리는 설문조사의 등록자 입장에서도, 설문조사의 참여자 입장에서도 모두 생각하며<br />
+    모두가 기분좋고 편리한 설문조사를 경험하도록 노력했습니다.</p><div className={styles.group351}><div className={styles.rec3}/><p className={styles.readMore}>Read More</p></div></div></div>);
+};
+
+export default Aboutblue;
